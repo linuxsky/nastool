7574e933658b3be1a2facab972e7808b8c2f1ed8
juway <1056349644@qq.com>
2023-05-25 18:19:10 +0000

    update 02_tar_docker.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- 02_tar_docker.sh
+++ 02_tar_docker.sh
@@ -17,7 +17,7 @@ green "==================================================="
 echo -e "你输入的下载目录为: \033[33m/volume$ip_address1/$ip_address2\033[0m"
 yellow "请确认目录是否正确"
 read -p "若目录正确，请按 Y 继续，按 N 重试 ：" y
-mkdir /volume$ip_address1/$ip_address2/dockerdown
+rm -rf /volume$ip_address1/$ip_address2/dockerdown && mkdir /volume$ip_address1/$ip_address2/dockerdown
 # 下载文件
 if [ $y = "y" ];then
     rm -rf /volume$ip_address1/$ip_address2/docker.tar.gz && wget -c https://d.nastool.work/d/%E9%98%BF%E9%87%8C%E7%BD%91%E7%9B%98/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E5%90%8C%E6%AD%A5/1.%E6%B8%B8%E5%AE%A2%E4%B8%8B%E8%BD%BD/SSH%E5%B7%A5%E5%85%B7/wget%E4%B8%8B%E8%BD%BD%E7%9B%AE%E5%BD%95/docker.tar.gz -O /volume$ip_address1/$ip_address2/docker.tar.gz
