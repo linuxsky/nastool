9b4d9542fe376d4c35b02aceff052f8f75a245f6
juway <1056349644@qq.com>
2023-05-25 08:48:14 +0000

    update 01_spk_download.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- 01_spk_download.sh
+++ 01_spk_download.sh
@@ -12,17 +12,18 @@ yellow() {
 #1.nastool
 nastool() {
 # 读取输入的目录和需要挂载的容器名
-read -p "请输入下载目录: " ip_address1
+read -p "请输入存储空间volume序号: " ip_address1
+read -p "请输入文件夹名称: " ip_address2
 # 确认目录是否正确
 green "==================================================="
-echo -e "你输入的下载目录为: \033[33m$ip_address1\033[0m"
+echo -e "你输入的下载目录为: \033[33m/volume$ip_address1/$ip_address2\033[0m"
 yellow "请确认目录是否正确"
 read -p "若目录正确，请按 Y 继续，按 N 重试 ：" y
 # 下载文件
 if [ $y = "y" ];then
-    rm -rf nastool_2.9.1_x64-DSM7.spk && wget -c https://d.nastool.work/d/%E9%98%BF%E9%87%8C%E7%BD%91%E7%9B%98/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E5%90%8C%E6%AD%A5/1.%E6%B8%B8%E5%AE%A2%E4%B8%8B%E8%BD%BD/SSH%E5%B7%A5%E5%85%B7/SSH%E5%B7%A5%E5%85%B7-%E5%88%86%E7%A6%BB%E5%A4%87%E4%BB%BD/nastool_2.9.1_x64-DSM7.spk -O $ip_address1/nastool_2.9.1_x64-DSM7.spk
+    rm -rf /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk && wget -c https://d.nastool.work/d/%E9%98%BF%E9%87%8C%E7%BD%91%E7%9B%98/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E5%90%8C%E6%AD%A5/1.%E6%B8%B8%E5%AE%A2%E4%B8%8B%E8%BD%BD/SSH%E5%B7%A5%E5%85%B7/SSH%E5%B7%A5%E5%85%B7-%E5%88%86%E7%A6%BB%E5%A4%87%E4%BB%BD/nastool_2.9.1_x64-DSM7.spk -O /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk
 # 检测是否下载
-if [ -f $ip_address1/nastool_2.9.1_x64-DSM7.spk ]; then
+if [ -f /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk ]; then
     yellow "下载已完成！"
 else
     yellow "下载失败，终止任务！"
@@ -35,10 +36,7 @@ else
     echo "输入非法指令"
 fi
 }
-#0.清除缓存
-rm_sh() {
-  rm -rf $ip_address1/nastool_2.9.1_x64-DSM7.spk
-}
+
 echo " ===================================================== "
 green "
 1.下载NasTool_v2.9套件
@@ -51,7 +49,7 @@ if [  $num  == 1  ] ; then
 nastool
 elif [  $num  == 0  ]; then
 red " 清除缓存中，请稍等..."
-rm_sh
+rm -rf /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk
 exit
 else
 red "请输入正确的数字，安装对应容器" 
