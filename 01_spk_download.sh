3d2e316e7f7cbecf5bab8dcd540c32d7a3d24baf
juway <1056349644@qq.com>
2023-05-25 09:07:32 +0000

    update 01_spk_download.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- 01_spk_download.sh
+++ 01_spk_download.sh
@@ -9,8 +9,6 @@ red() {
 yellow() {
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
-#1.nastool
-nastool() {
 # 读取输入的目录和需要挂载的容器名
 read -p "请输入存储空间volume序号: " ip_address1
 read -p "请输入文件夹名称: " ip_address2
@@ -25,9 +23,6 @@ if [ $y = "y" ];then
 # 检测是否下载
 if [ -f /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk ]; then
     yellow "下载已完成！"
-    echo "#!/bin/bash
-rm -rf /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk" >> /volume$ip_address1/$ip_address2/rm_spk.sh
-    bash <(curl https://gitee.com/juway111/nastool/raw/master/01_spk_download.sh)
 else
     yellow "下载失败，终止任务！"
     exit 1
@@ -38,27 +33,11 @@ elif [ $y = "n" ];then
 else
     echo "输入非法指令"
 fi
-}
 #0.清除缓存
-rm_sh() {
-  rm -rf rm_spk.sh && chmod 777 rm_spk.sh && bash rm_spk.sh && rm -rf rm_spk.sh
-}
-echo " ===================================================== "
-green "
-1.下载NasTool_v2.9套件
-0.清除缓存
-
-======================================================"
-
-read -p  " 请输入以上数字，安装对应容器: " num 
-if [  $num  == 1  ] ; then
-nastool
-elif [  $num  == 0  ]; then
-red " 清除缓存中，请稍等..."
-rm -rf /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk
-exit
+yellow "已下载，等待清除缓存..."
+read -p "按 Y 清除缓存 ：" y
+if [ $y = "y" ];then
+    rm -rf /volume$ip_address1/$ip_address2/nastool_2.9.1_x64-DSM7.spk
 else
-red "请输入正确的数字，安装对应容器" 
-yellow "请输入正确的数字，安装对应容器 " 
-green "请输入正确的数字，安装对应容器" 
+exit 1
 fi
