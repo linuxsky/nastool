5d1e213e27618e8c300f7341aa5f83b756650afa
juway <1056349644@qq.com>
2023-05-26 07:54:32 +0000

    update 00_bash_start.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- 00_bash_start.sh
+++ 00_bash_start.sh
@@ -13,15 +13,23 @@ yellow() {
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-if which wget > /dev/null 2>&1  
-then  
-# 1.查看本机ipv4公网IP
+if which wget > /dev/null 2>&1 
+then
+# 1.群晖、威联通、unraid等nas系统
 nas() {
     bash <(curl https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh)
 }
-# 2.自动修改hosts文件
+# 2.centos服务器系统
 centos() {
     bash <(curl https://gitee.com/juway111/nastool/raw/master/centos/00_centos_start.sh)
+}
+# 3.查看docker整体磁盘使用率
+docker system df() {
+    docker system df
+}
+# 4.删除未被任何容器使用的本地卷
+docker volume prune() {
+    docker volume prune
 }
 
 #内存使用率
@@ -38,16 +46,21 @@ echo -e "
 CPU平均使用率:$my_cpu
 ====================================================="
 echo -e "\033[32m1.群晖、威联通、unraid等nas系统
-2.centos服务器系统
+2.centos服务器系统.
+3.查看docker整体磁盘使用率
+4.删除未被任何容器使用的本地卷
 \033[32m0.退出脚本\033[0m
 ======================================================"
-rm -rf ./gongju.sh.*
-rm -rf ./speedtest-cli
+
 read -p  " 请输入以上数字[0-4]查看系统相应信息: " num
-if [  $num  == 1  ] ; then
+if [  $num  == 1  ]; then
 nas
 elif [  $num  == 2  ]; then
 centos
+elif [  $num  == 3  ]; then
+docker system df
+elif [  $num  == 4  ]; then
+docker volume prune
 elif [  $num  == 0  ]; then
 red " 我们下次再见，拜拜"
 exit
@@ -55,7 +68,8 @@ else
 red "请输入正确的数字，启动对应功能[0-4]: "
 yellow "请输入正确的数字，启动对应功能[0-4]: "
 green "请输入正确的数字，启动对应功能[0-4]: "
+bash <(curl https://gitee.com/juway111/nastool/raw/master/00_bash_start.sh)
 fi
-else  
+else
   yum -y install wget
 fi
