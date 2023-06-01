345f74206792b44680f2479cc7f5a3e256c4fb6f
juway <1056349644@qq.com>
2023-06-01 08:13:57 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 下载ffmpeg
+mkdir ffmpeg
 rm -rf ffmpeg-git-amd64-static.tar.xz && wget "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz" && tar -xvf ffmpeg-git-amd64-static.tar.xz -C ffmpeg && rm -rf ffmpeg-git-amd64-static.tar.xz
 # 移动目录
 cp ffmpeg/ffmpeg /usr/bin/
