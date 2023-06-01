34906090325771a7c97b52f0e7a8e2e7ce372eb7
juway <1056349644@qq.com>
2023-06-01 10:34:01 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -13,18 +13,17 @@ yellow() {
 # 下载ffmpeg  
 rm -rf /root/ffmpeg        # 删除目录
 mkdir /root/ffmpeg         # 创建目录  
-wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
-if [ -f ffmpeg-git-amd64-static.tar.xz ]; then  
-rm -rf ffmpeg-git-amd64-static.tar.xz   # 删除原文件  
+wget -O ffmpeg-git-amd64-static.tar.xz https://12312312312johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 
+if [ -f ffmpeg-git-amd64-static.tar.xz ]; then  
+tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
 red "*****************************************"
 filename=$(ls -p /root/ffmpeg)  
 green "jellyfi控制台设置FFmpeg路径设置为："
 yellow "/ffmpeg/$filename"ffmpeg
 red "*****************************************"
-    exit 1  
+sleep 2
+rm -rf ffmpeg-git-amd64-static.tar.xz   # 删除原文件  
 else
-    bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/02_ffmpeg.sh)
-    yellow "正在重试一次..."
+    yellow "下载失败，再试一次吧！"
 fi
