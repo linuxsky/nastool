fe9bae35c4b947185af3d46c864bc4bf6527f75a
juway <1056349644@qq.com>
2023-06-01 09:02:46 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -16,11 +16,16 @@ mkdir /root/temp         # 创建/root/temp目录
 wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz   # wget下载ffmpeg，保存到ffmpeg-git-amd64-static.tar.xz  
 tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/temp   # 解压缩到/root/temp目录  
 rm -rf ffmpeg-git-amd64-static.tar.xz   # 删除原文件  
-  
+
+if [ -f ffmpeg-git-amd64-static.tar.xz ]; then
 if [ -d "/root/temp" ]; then   # 如果/root/temp目录存在  
     find /root/temp -depth -name "ffmpeg*" -o -name "ffprobe*" -exec cp -i '{}' /usr/bin/ \;   # 在/root/temp目录下查找ffmpeg和ffprobe文件，并复制到/usr/bin/目录  
     yellow "任务完成！"   # 完成任务，输出黄色提示  
 else  
     yellow "解压失败，终止任务！"   # 如果没有/root/temp目录，输出黄色提示并终止任务  
     exit 1  
+fi
+else
+    bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/02_ffmpeg.sh)
+    yellow "正在重试一次..."
 fi
