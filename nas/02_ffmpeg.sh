f44073403d2e8044554482484b6be47069a9a13a
juway <1056349644@qq.com>
2023-06-01 08:38:43 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 下载ffmpeg
-mkdir /root/temp
-rm -rf ffmpeg-git-amd64-static.tar.xz
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar -xvf ffmpeg-git-amd64-static.tar.xz -C /root/temp
-rm -rf ffmpeg-git-amd64-static.tar.xz
-find /temp -depth -name "ffmpeg*" -o -name "ffprobe*" -exec cp -i '{}' /usr/bin/ \;
+if mkdir /root/temp,then
+rm -rf ffmpeg-git-amd64-static.tar.xz,then
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz,then
+tar -xvf ffmpeg-git-amd64-static.tar.xz -C /root/temp,then
+rm -rf ffmpeg-git-amd64-static.tar.xz,then
+find /root/temp -depth -name "ffmpeg*" -o -name "ffprobe*" -exec cp -i '{}' /usr/bin/ \;
