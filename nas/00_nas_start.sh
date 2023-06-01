67645df61f12d1d88ab58cfe9c2a5c0c84cf900c
juway <1056349644@qq.com>
2023-06-01 10:41:25 +0000

    update nas/00_nas_start.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/00_nas_start.sh
+++ nas/00_nas_start.sh
@@ -57,7 +57,7 @@ fi
 ffmpeg() {
 rm -rf /root/ffmpeg        # 删除目录
 mkdir /root/ffmpeg         # 创建目录  
-wget -O ffmpeg-git-amd64-static.tar.xz https://12312312312johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 
 if [ -d "/ffmpeg/$filename" ]; then  
 tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
