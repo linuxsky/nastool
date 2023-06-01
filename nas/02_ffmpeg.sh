a358aca77380ff9287ab31d59a095f6a39e98545
juway <1056349644@qq.com>
2023-06-01 10:42:17 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash  
 
+# 这个是副本，需要同步更新才可以
 # 增加颜色
 green() {
 	echo -e "\033[32m\033[01m$1\033[0m"
@@ -13,7 +14,7 @@ yellow() {
 # 下载ffmpeg  
 rm -rf /root/ffmpeg        # 删除目录
 mkdir /root/ffmpeg         # 创建目录  
-wget -O ffmpeg-git-amd64-static.tar.xz https://12312312312johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 
 if [ -d "/ffmpeg/$filename" ]; then  
 tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
