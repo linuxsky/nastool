7b4503576fed37ec862c012b6c33e053d27db6b4
juway <1056349644@qq.com>
2023-06-01 09:59:17 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -11,21 +11,16 @@ yellow() {
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 # 下载ffmpeg  
-rm -rf /root/tempffmpeg        # 删除/root/temp目录（如果不存在）  
-mkdir /root/tempffmpeg         # 创建/root/temp目录  
-wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz   # wget下载ffmpeg，保存到ffmpeg-git-amd64-static.tar.xz  
-tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/tempffmpeg   # 解压缩到/root/temp目录
+rm -rf /root/ffmpeg        # 删除目录
+mkdir /root/ffmpeg         # 创建目录  
+wget -O ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
 if [ -f ffmpeg-git-amd64-static.tar.xz ]; then  
 rm -rf ffmpeg-git-amd64-static.tar.xz   # 删除原文件  
-
-if [ -f /usr/bin/ffmpeg -a -f /usr/bin/ffprobe ]; then   # 如果文件存在  
-    find /root/tempffmpeg -depth -name "ffmpeg*" -o -name "ffprobe*" -exec cp -i '{}' /usr/bin/ \;   # 在/root/temp目录下查找ffmpeg和ffprobe文件，并复制到/usr/bin/目录  
-    yellow "/usr/bin/目录下已经存在ffmpeg和ffprobe，任务完成！"
-else  
-    yellow "解压失败，终止任务！"
+yellow "*****************************************"
+    yellow "jellyfi控制台设置FFmpeg路径设置为/ffmpeg/"&ls /root/ffmpeg&yellow "/ffmpeg"
+yellow "*****************************************"
     exit 1  
-fi
-
 else
     bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/02_ffmpeg.sh)
     yellow "正在重试一次..."
