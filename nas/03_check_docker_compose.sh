6294d51c6e6a638111cfa5a93742d4441fa37305
juway <1056349644@qq.com>
2023-06-02 03:14:46 +0000

    update nas/03_check_docker_compose.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/03_check_docker_compose.sh
+++ nas/03_check_docker_compose.sh
@@ -10,6 +10,11 @@ if ! [[ $result == *"docker-compose"* ]]; then
   sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   docker-compose --version
+if [ -f /usr/local/bin/docker-compose ]; then 
+  echo "安装成功！"
+else  
+  echo "安装失败！"
+fi
 else  
   echo "docker-compose already installed"  
 fi
