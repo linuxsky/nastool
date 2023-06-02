dcd86570eb13c7bda9175e7b827d3bb3db3d360d
juway <1056349644@qq.com>
2023-06-02 02:56:19 +0000

    update 1.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- 1.sh
+++ 1.sh
@@ -2,9 +2,10 @@
   
 # 检查docker-compose是否已安装  
 result=$(docker-compose -v)  
-# 检查输出是否包含版本信息  
-if ! [[ $result == *"docker-compose version:"*" " ]]; then  
-  # 如果不包含版本信息，则安装docker-compose  
+  
+# 检查输出是否包含"docker-compose"字符串  
+if ! [[ $result == *"docker-compose"* ]]; then  
+  # 如果不包含"docker-compose"字符串，则安装docker-compose  
   echo "Installing docker-compose..."  
 else  
   echo "docker-compose already installed"  
