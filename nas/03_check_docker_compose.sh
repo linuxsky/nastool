2116d28c8a235d736b65e587b2346ce19de250dc
juway <1056349644@qq.com>
2023-06-02 03:48:41 +0000

    update nas/03_check_docker_compose.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/03_check_docker_compose.sh
+++ nas/03_check_docker_compose.sh
@@ -11,14 +11,17 @@ if ! [[ $result == *"docker-compose"* ]]; then    # 如果不包含"docker-compo
   docker-compose --version
 if [ -f /usr/local/bin/docker-compose ]; then 
   echo "安装成功！正在跳转..."
+  rm -rf check.sh
   sleep 1
   bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh)
   bash 00_nas_start.sh
 else  
   echo "安装失败，请重试！"
+  exit 1
 fi
 else  
   echo "docker-compose 已经安装！正在跳转..."  
+  rm -rf check.sh
   sleep 1
   bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh)
   bash 00_nas_start.sh
