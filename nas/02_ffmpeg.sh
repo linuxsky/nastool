abcb6e445d7a3ac6f889fb96649106de2f1adc05
juway <1056349644@qq.com>
2023-06-02 07:09:50 +0000

    update nas/02_ffmpeg.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/02_ffmpeg.sh
+++ nas/02_ffmpeg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash  
 
-# 这个是副本，需要同步更新才可以
 # 增加颜色
 green() {
 	echo -e "\033[32m\033[01m$1\033[0m"
@@ -11,21 +10,22 @@ red() {
 yellow() {
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
+read -p "请输入你的docker目录: " ip_address1
+mkdir $ip_address1/jellyfin/ffmpeg
+
 # 下载ffmpeg  
-rm -rf /root/ffmpeg        # 删除目录
-mkdir /root/ffmpeg         # 创建目录  
 wget -O ffmpeg-git-amd64-static.tar.xz https://123123johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar -xJvf ffmpeg-git-amd64-static.tar.xz -C $ip_address1/jellyfin/ffmpeg   # 解压缩到目录
 
 # 指定要查找的文件名和目录路径  
 file_to_find="ffmpeg"  
-directory="/root/ffmpeg"  
+directory="$ip_address1/jellyfin/ffmpeg"  
   
 # 遍历目录下的所有文件和子文件夹  
 for file in $(find "$directory" -type f -name "$file_to_find")  
 do  
   # 判断文件是否存在  
   if [ -f "$file" ]; then  
-tar -xJvf ffmpeg-git-amd64-static.tar.xz -C /root/ffmpeg   # 解压缩到目录
 red "*****************************************"
 filename=$(ls -p /root/ffmpeg)  
 green "jellyfi控制台设置FFmpeg路径设置为："
