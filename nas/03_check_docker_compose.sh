d7268170145f685521c5bed92e0a42c2dacb379e
juway <1056349644@qq.com>
2023-06-02 03:30:42 +0000

    update nas/03_check_docker_compose.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/03_check_docker_compose.sh
+++ nas/03_check_docker_compose.sh
@@ -4,8 +4,7 @@
 result=$(docker-compose -v)  
   
 # 检查输出是否包含"docker-compose"字符串  
-if ! [[ $result == *"docker-compose"* ]]; then  
-  # 如果不包含"docker-compose"字符串，则安装docker-compose  
+if ! [[ $result == *"docker-compose"* ]]; then    # 如果不包含"docker-compose"字符串，则安装docker-compose  
   echo "Installing docker-compose..."  
   sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
@@ -19,7 +18,8 @@ else
   echo "安装失败，请重试！"
 fi
 else  
-  echo "docker-compose already installed"  
+  echo "docker-compose 已经安装！正在跳转..."  
+  sleep 1
   bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh)
   bash 00_nas_start.sh
 fi
