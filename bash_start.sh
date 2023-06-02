d4c9f6716f72b40aeefa1697d6d8c1e985e39842
juway <1056349644@qq.com>
2023-06-02 02:09:37 +0000

    update bash_start.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- bash_start.sh
+++ bash_start.sh
@@ -14,19 +14,21 @@ yellow() {
 }
 # 检查wget是否安装
 if which wget > /dev/null 2>&1; then
-# 1.群晖、威联通、unraid等nas系统
+# 1.群晖、unraid等nas系统
 nas() {
 	bash <(curl -s https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh)
 }
-# 2.centos服务器系统
+# 2.威联通系统
+	wget https://gitee.com/juway111/nastool/raw/master/nas/00_nas_start.sh && bash 00_nas_start.sh && rm -rf 00_bash_start.sh
+# 3.centos服务器系统
 centos() {
 	bash <(curl -s https://gitee.com/juway111/nastool/raw/master/centos/00_centos_start.sh)
 }
-# 3.查看docker整体磁盘使用率
+# 4.查看docker整体磁盘使用率
 df() {
 	docker system df
 }
-# 4.删除未被任何容器使用的本地卷
+# 5.删除未被任何容器使用的本地卷
 prune() {
 	docker volume prune -y
 }
@@ -43,31 +45,35 @@ echo -e "
 内存使用率:$my_mem
 CPU平均使用率:$my_cpu
 ====================================================="
-echo -e "\033[32m1.群晖、威联通、unraid等nas系统
-2.centos服务器系统
-3.查看docker整体磁盘使用率
-4.删除未被任何容器使用的本地卷
-\033[32m0.退出脚本\033[0m
+echo -e "\033[32m1.群晖、unraid等nas系统
+2.威联通等nas系统
+3.centos服务器系统
+4.查看docker整体磁盘使用率
+5.删除未被任何容器使用的本地卷
+\033[32m0.清除缓存并退出脚本\033[0m
 ======================================================"
 
 read -p "请输入以上数字[0-4]查看系统相应信息: " num
 if [  $num  == 1  ]; then
 nas
 elif [  $num  == 2  ]; then
-centos
+weiliantong
 elif [  $num  == 3  ]; then
-df
+centos
 elif [  $num  == 4  ]; then
+df
+elif [  $num  == 5  ]; then
 prune
 elif [  $num  == 0  ]; then
 red "我们下次再见，拜拜 "
+rm -rf 00_bash_start.sh
 exit 1
 else
-red "请输入正确的数字，启动对应功能[0-4]: "
-yellow "请输入正确的数字，启动对应功能[0-4]: "
-green "请输入正确的数字，启动对应功能[0-4]: "
+red "请输入正确的数字，启动对应功能[0-5]: "
+yellow "请输入正确的数字，启动对应功能[0-5]: "
+green "请输入正确的数字，启动对应功能[0-5]: "
 fi
 else
 	yum -y install wget
 fi
-bash <(curl -s https://gitee.com/juway111/nastool/raw/master/00_bash_start.sh)
+bash 00_bash_start.sh
