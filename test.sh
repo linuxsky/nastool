5221e9d7efe9ac75df7a5312dc598da65e0969e2
juway <1056349644@qq.com>
2023-06-10 01:45:20 +0000

    update test.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash  
   
+mkdir /temp
+
+cd /temp
+
+wget http://ipkg.nslu2-linux.org/feeds/optware/syno-i686/cross/unstable/syno-i686-bootstrap_1.2-7_i686.xsh
+
+
+chmod +x syno-i686-bootstrap_1.2-7_i686.xsh
+
+sh syno-i686-bootstrap_1.2-7_i686.xsh
+
+ipkg update
+ipkg install lm-sensors
+sensors
