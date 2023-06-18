d27e8ca312df4b74395f184e91f93751b873c7f1
juway <1056349644@qq.com>
2023-06-18 05:40:25 +0000

    update nas/03_check_docker_compose.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/03_check_docker_compose.sh
+++ nas/03_check_docker_compose.sh
@@ -10,7 +10,7 @@ result=$(docker-compose -v)
 # 检查输出是否包含"docker-compose"字符串  
 if ! [[ $result == *"docker-compose"* ]]; then    # 如果不包含"docker-compose"字符串，则安装docker-compose  
   yellow "正在安装 docker-compose..."  
-  sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   docker-compose --version
 if [ -f /usr/local/bin/docker-compose ]; then 
