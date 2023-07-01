2a61af11535e487dd85f40f178100135a5ce231b
juway <1056349644@qq.com>
2023-07-01 03:36:31 +0000

    update nas/03_check_docker_compose.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/03_check_docker_compose.sh
+++ nas/03_check_docker_compose.sh
@@ -10,7 +10,7 @@ result=$(docker-compose -v)
 # 检查输出是否包含"docker-compose"字符串  
 if ! [[ $result == *"version"* ]]; then    # 如果不包含"version"字符串，则安装docker-compose  
   yellow "正在安装 docker-compose..."  
-  wget https://d.nastool.work/d/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E5%90%8C%E6%AD%A5/1.%E6%B8%B8%E5%AE%A2%E4%B8%8B%E8%BD%BD/SSH%E5%B7%A5%E5%85%B7/wget%E4%B8%8B%E8%BD%BD%E7%9B%AE%E5%BD%95/docker-compose-linux-x86_64 -O /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+  wget https://nastool.work/bash/wget/docker-compose-linux-x86_64 -O /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
   docker-compose --version
 if [ -f /usr/local/bin/docker-compose ]; then 
   yellow "安装成功！正在跳转..."
