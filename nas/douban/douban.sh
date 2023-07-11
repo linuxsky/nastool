216d3f686a3f258d0b1250aca5306c6d95d4b36c
juway <1056349644@qq.com>
2023-07-11 08:44:38 +0000

    douban.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/douban/douban.sh
+++ nas/douban/douban.sh
@@ -1,11 +1,35 @@
 #!/bin/bash  
-  
-# 检查docker-compose是否已安装  
-result=$(docker-compose -v)  
-# 检查输出是否包含版本信息  
-if ! [[ $result == *"docker-compose version:"*" " ]]; then  
-  # 如果不包含版本信息，则安装docker-compose  
-  echo "Installing docker-compose..."  
-else  
-  echo "docker-compose already installed"  
+
+# 定义颜色
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
+}
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
+}
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+read -p "请先确认版本，2.9请按数字“2”，3.2请按数字“3”：" y
+if [ $y = "2" ];then
+# 下载文件
+    rm -rf nas-tools-2.9.1.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-2.9.1.tar.gz -O nas-tools-2.9.1.tar.gz
+if [ -f nas-tools-2.9.1.tar.gz ]; then
+    yellow "下载已完成！"
+else
+    yellow "下载失败，终止任务！"
+    exit 1
+fi
+yellow "请查看nastool的docker容器名称"
+docker ps
+read -p "请输入你的docker容器名称：" y2
+tar -zxf nas-tools-2.9.1.tar.gz -C $y2:/
+if [ -d "$y2:/web" ]; then
+    yellow "解压缩成功，文件已经存储docker目录中"
+    rm -rf nas-tools-2.9.1.tar.gz
+    else
+    yellow "解压失败，终止任务！"
+    exit 1
+fi
 fi
