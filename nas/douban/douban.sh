bc757c633082600ac0909bfdf54fc8b31c67b5e4
juway <1056349644@qq.com>
2023-07-11 09:26:16 +0000

    update nas/douban/douban.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/douban/douban.sh
+++ nas/douban/douban.sh
@@ -11,8 +11,8 @@ yellow() {
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-read -p "请先确认版本，2.9请按数字“2”，3.2请按数字“3”：" y
-if [ $y = "2" ];then
+read -p "请先确认版本，2.9请按数字“2.9”，3.2请按数字“3.2”：" y
+if [ $y = "2.9" ];then
 # 下载文件
     rm -rf nas-tools-2.9.1.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-2.9.1.tar.gz -O nas-tools-2.9.1.tar.gz
 if [ -f nas-tools-2.9.1.tar.gz ]; then
@@ -21,15 +21,39 @@ else
     yellow "下载失败，终止任务！"
     exit 1
 fi
-yellow "请查看nastool的docker容器名称"
+yellow "请查看nastool的docker容器ID"
 docker ps
-read -p "请输入你的docker容器名称：" y2
+yellow "*************************************"
+read -p "请输入你的docker容器ID：" y2
 rm -r /douban/ && mkdir /douban/
 tar -zxf nas-tools-2.9.1.tar.gz -C /douban/
 docker cp -a /douban/nas-tools $y2:/
 if [ -d "/douban/nas-tools" ]; then
-    yellow "解压缩成功，文件已经存储docker目录中"
+    yellow "恭喜natool豆瓣api已经修复，重启容器生效"
     rm -rf nas-tools-2.9.1.tar.gz
+    else
+    yellow "解压失败，终止任务！"
+    exit 1
+fi
+if [ $y = "3.2" ];then
+# 下载文件
+    rm -rf nas-tools-3.2.3.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-3.2.3.tar.gz -O nas-tools-3.2.3.tar.gz
+if [ -f nas-tools-3.2.3.tar.gz ]; then
+    yellow "下载已完成！"
+else
+    yellow "下载失败，终止任务！"
+    exit 1
+fi
+yellow "请查看nastool的docker容器ID"
+docker ps
+yellow "*************************************"
+read -p "请输入你的docker容器ID：" y2
+rm -r /douban/ && mkdir /douban/
+tar -zxf nas-tools-3.2.3.tar.gz -C /douban/
+docker cp -a /douban/nas-tools $y2:/
+if [ -d "/douban/nas-tools" ]; then
+    yellow "恭喜natool豆瓣api已经修复，重启容器生效"
+    rm -rf nas-tools-3.2.3.tar.gz
     else
     yellow "解压失败，终止任务！"
     exit 1
