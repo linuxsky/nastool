ea31d596d0f26ea24b3b88f2636bfdb0179113e4
juway <1056349644@qq.com>
2023-07-14 16:38:37 +0000

    update nas/douban/douban.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/douban/douban.sh
+++ nas/douban/douban.sh
@@ -12,10 +12,9 @@ yellow() {
 }
 
 # 下载文件
-rm -rf nas-tools-2.9.1.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-2.9.1.tar.gz -O nas-tools-2.9.1.tar.gz && rm -rf /douban/ && rm -rf douban.sh&& mkdir -p /douban/ && tar -zxf nas-tools-2.9.1.tar.gz -C /douban/ && docker cp -a "/douban/nas-tools" $container_id:/
+container_id=$(docker ps -aqf "name=01-nastools-bt") && rm -rf nas-tools-2.9.1.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-2.9.1.tar.gz -O nas-tools-2.9.1.tar.gz && rm -rf /douban/ && rm -rf douban.sh&& mkdir -p /douban/ && tar -zxf nas-tools-2.9.1.tar.gz -C /douban/ && docker cp -a "/douban/nas-tools" $container_id:/
 
 yellow "*************************************"
-container_id=$(docker ps -aqf "name=01-nastools-bt")
 echo "容器名称：01-nastools-bt"
 echo "容器ID: $container_id"
 yellow "*************************************"
