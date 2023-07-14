0b900c648de6b48fbbed2a15146b27af197415d0
juway <1056349644@qq.com>
2023-07-14 16:34:07 +0000

    update nas/douban/douban.sh.
    Signed-off-by: juway <1056349644@qq.com>

--- nas/douban/douban.sh
+++ nas/douban/douban.sh
@@ -13,12 +13,6 @@ yellow() {
 
 # 下载文件
 rm -rf nas-tools-2.9.1.tar.gz && wget -c https://gitee.com/juway111/nastool/raw/master/nas/douban/nas-tools-2.9.1.tar.gz -O nas-tools-2.9.1.tar.gz
-if [ -f nas-tools-2.9.1.tar.gz ]; then
-    yellow "下载已完成！"
-else
-    yellow "下载失败，终止任务！"
-    exit 1
-fi
 
 yellow "*************************************"
 container_id=$(docker ps -aqf "name=01-nastools-bt")
@@ -31,8 +25,8 @@ tar -zxf nas-tools-2.9.1.tar.gz -C /douban/
 docker cp -a "/douban/nas-tools" $container_id:/
 if [ -d "/douban/nas-tools" ]; then
     green "恭喜natool豆瓣api已经修复，正在重启容器..."
-    docker restart $container_id
-    rm -rf nas-tools-2.9.1.tar.gz
+docker restart $container_id
+rm -rf nas-tools-2.9.1.tar.gz
     else
     red "解压失败，终止任务！"
     exit 1
