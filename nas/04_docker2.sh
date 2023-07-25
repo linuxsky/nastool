35bf2f7e331882125bb20ff7769232e2f02d155d
juway <1056349644@qq.com>
2023-07-25 16:41:12 +0000

    修改脚本描述词
    Signed-off-by: juway <1056349644@qq.com>

--- nas/04_docker2.sh
+++ nas/04_docker2.sh
@@ -92,11 +92,11 @@ rm_sh() {
 
 echo " ===================================================== "
 green "
-1.NasTool_v2.9版本
+1.NasTool_v2.9.1_优化版（修复豆瓣API）
 2.TR下载器
 3.Jellyfin播放器
 4.TMM搜刮器 "
-yellow "5.全部安装Nastool_v2.9版本6款软件 "
+yellow "5.全部安装Nastool_v2.9_优化版6款软件（修复豆瓣API）"
 green "6.NasTool_v3.0+版本（修复豆瓣API） "
 yellow "7.全部安装Nastool_v3.0+版本5款软件（修复豆瓣API） "
 green "8.jackett资源池 "
