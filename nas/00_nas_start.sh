38e1f571788255bb3561ad87f9080fd3ff59bf54
Fork <1056349644@qq.com>
2023-08-09 07:49:28 +0000

    update nas/00_nas_start.sh.
    Signed-off-by: Fork <1056349644@qq.com>

--- nas/00_nas_start.sh
+++ nas/00_nas_start.sh
@@ -113,7 +113,7 @@ elif [  $num  == 99  ]; then
     rm_images_container
 elif [  $num  == 0  ]; then
 	red "我们下次再见，拜拜"
-	exit 1 && rm -rf 00_nas_start.sh
+	exit && rm -rf 00_nas_start.sh
 else
 	red "****输入错误！请输入正确的数字，启动对应功能！****"
 fi
