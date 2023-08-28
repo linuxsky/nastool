0101b8db2a1a20fe691a51b1ce468d2d90913b66
Fork <1056349644@qq.com>
2023-08-28 03:07:42 +0000

    update 测试用脚本.sh.
    Signed-off-by: Fork <1056349644@qq.com>

--- 测试用脚本.sh
+++ 测试用脚本.sh
@@ -1,15 +1,6 @@
 #!/bin/bash  
   
-mkdir /temp && cd /temp
-
-wget http://ipkg.nslu2-linux.org/feeds/optware/syno-i686/cross/unstable/syno-i686-bootstrap_1.2-7_i686.xsh
-
-chmod +x syno-i686-bootstrap_1.2-7_i686.xsh
-
-sh syno-i686-bootstrap_1.2-7_i686.xsh
-
-ipkg update
-
-ipkg install lm-sensors
-
-sensors
+read -s -p "Enter your password: " password  
+hash=$(echo -n "$password" | sha256sum | awk '{print $1}')  
+  
+echo "Your password hash is: $hash"
